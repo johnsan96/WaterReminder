a9a5396bfdc490ce99d4617ddc5732c7713668f8
john96vrs <johndiggle@web.de>
2023-04-06 01:57:42 +0200

    setup svg

--- App.tsx
+++ App.tsx
@@ -17,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2a2a2a',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- views/Home.tsx
+++ views/Home.tsx
@@ -1,13 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
 import { DEV_MODE, YALA_MODE } from "@env";
+import Toothwheel from "../assets/toothweel.svg"
 
 export default function Home() {
 
     return (
-        <View>
+        <View style={{ backgroundColor: '#2A2A2A' }}>
             <Text>Home</Text>
+            <Toothwheel />
             <Text>yo</Text>
-            <Text>Url: {YALA_MODE}</Text> 
+            <Text>Url: {DEV_MODE}</Text> 
         </View>
     );
 }
